c9096bb41001e090a8335c6e7e7dfac45a0d301e
Elbin12 <elbinshiby14@gmail.com>
2025-08-06 19:23:47 +0530

    TransmitSMSAccount model created; urls, serializers, views added;

--- transmitsms/views.py
+++ transmitsms/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
+from .models import TransmitSMSAccount
+from .serializers import TransmitSMSAccountSerializer
 # Create your views here.
+
+class TransmitSMSAccountViewSet(viewsets.ModelViewSet):
+    queryset = TransmitSMSAccount.objects.all()
+    serializer_class = TransmitSMSAccountSerializer
