dfffe7241956cd1c645324c12dd33942d129c533
Rojin4455 <rojinsaji35@gmail.com>
2025-08-06 19:22:04 +0530

    added ghl onboarding and logins

--- reloopsms/urls.py
+++ reloopsms/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('core/', include("core.urls")),
+    path('api/admin/', admin.site.urls),
+    path('api/core/', include("core.urls")),
 ]
